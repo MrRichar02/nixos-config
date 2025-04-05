84a7c96dde528fc94a786b086b5d4ddf4b5c12b8
MrRichar02 <richarmamadi03@gmail.com>
2025-04-05 18:45:51 -0500

    changing catppuccin stylix to gruvbox

--- configuration.nix
+++ configuration.nix
@@ -112,7 +112,7 @@
 
   #stylix conf
 
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
 
 
