7ce83ebbbbee54a6511ffc2cd85ce29088a057c9
MrRichar02 <richarmamadi03@gmail.com>
2025-04-05 21:46:17 -0500

    adding stylix config for fonts

--- stylix.nix
+++ stylix.nix
@@ -1,11 +1,33 @@
-{ pkgs, ...}:
+{ pkgs, inputs, ...}:
 {
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 16;
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+    };
+
   };
+
 }
+
