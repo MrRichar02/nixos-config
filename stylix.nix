d12d4cc1fe5873f823cab2bdcca05a23437f3d8a
MrRichar02 <richarmamadi03@gmail.com>
2025-04-05 22:06:44 -0500

    adding config for font size and prefer mode (dark)

--- stylix.nix
+++ stylix.nix
@@ -25,7 +25,23 @@
         package = pkgs.dejavu_fonts;
         name = "DejaVu Serif";
       };
+
+      sizes = {
+        applications = 12;
+        terminal = 15;
+        desktop = 10;
+        popups = 10;
+      };
     };
+
+    opacity = {
+      applications = 1.0;
+      terminal = 1.0;
+      desktop = 1.0;
+      popups = 1.0;
+    };
+
+    polarity = "dark";
 
   };
 
