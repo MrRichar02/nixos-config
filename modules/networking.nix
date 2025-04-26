1e4f84dff22e0409377774745d79a4b9bef7dba7
MrRichar02 <richarmamadi03@gmail.com>
2025-04-26 11:01:39 -0500

    change in how to manage dns servers

--- modules/networking.nix
+++ modules/networking.nix
@@ -11,6 +11,8 @@
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
     # Enable networking
     networking.networkmanager.enable = true;
-    networking.networkmanager.insertNameservers = [ "1.1.1.1" "8.8.8.8"];
+    networking.networkmanager.dns = "none";
+    networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    #networking.networkmanager.insertNameservers = [ "1.1.1.1" "8.8.8.8"];
   };
 }
