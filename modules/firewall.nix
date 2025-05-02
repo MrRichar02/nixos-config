1f9e05103b532c449a4df8915d4c29c36ad26617
MrRichar02 <richarmamadi03@gmail.com>
2025-05-01 19:41:09 -0500

    enabling interface trafic

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -4,4 +4,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.trustedInterfaces = [ "enp1s0" ];
 }
