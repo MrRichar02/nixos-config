06f35162cc9df8b33d76955b8316085b63748735
MrRichar02 <richarmamadi03@gmail.com>
2025-05-01 19:44:12 -0500

    adding interface to trusted

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -4,5 +4,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking.firewall.trustedInterfaces = [ "enp1s0" ];
+  networking.firewall.trustedInterfaces = [ "enp1s0" "virbr0" ];
 }
