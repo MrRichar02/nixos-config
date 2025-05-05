d0147808e6a873ff774fa9b272d8f3d0c8aa559f
MrRichar02 <richarmamadi03@gmail.com>
2025-05-04 22:36:27 -0500

    adding more config for zsh

--- modules/shells/zsh/default.nix
+++ modules/shells/zsh/default.nix
@@ -5,6 +5,10 @@
   config = lib.mkIf config.myModules.shell.zsh.enable {
     programs.zsh = {
       enable = true;
+      enableCompletions = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
     };
   };
 }
