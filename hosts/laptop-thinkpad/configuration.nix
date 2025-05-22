8b890907965324ba6cdee64ce50ebc3a8d61de4a
MrRichar02 <richarmamadi03@gmail.com>
2025-05-22 13:05:29 -0500

    disabling gpu-recorder

--- hosts/laptop-thinkpad/configuration.nix
+++ hosts/laptop-thinkpad/configuration.nix
@@ -33,9 +33,6 @@
     podman.enable = false;
   };
 
-  programs.gpu-screen-recorder.enable = true;
-
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
