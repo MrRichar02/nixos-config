f9fc305707a5a5cedf80054d7caa836a45584e8c
MrRichar02 <richarmamadi03@gmail.com>
2025-05-26 16:35:40 -0500

    configuring opengl

--- hosts/laptop-thinkpad/configuration.nix
+++ hosts/laptop-thinkpad/configuration.nix
@@ -33,6 +33,14 @@
     podman.enable = false;
   };
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-driver
+      intel-ocl
+    ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
