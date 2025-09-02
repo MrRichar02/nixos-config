bc3ae24436059464018e3ff27b91407e72353683
MrRichar02 <richarmamadi03@gmail.com>
2025-09-01 21:28:37 -0500

    changing cursor

--- modules/style/default.nix
+++ modules/style/default.nix
@@ -1,58 +1,59 @@
-{ pkgs, lib, config, ...}:
 {
-    options.myModules.stylix.enable = lib.mkEnableOption "enables stylix module";
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.myModules.stylix.enable = lib.mkEnableOption "enables stylix module";
 
-    config = lib.mkIf config.myModules.stylix.enable {
-        stylix = {
-            enable = true;
-            base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-            # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  config = lib.mkIf config.myModules.stylix.enable {
+    stylix = {
+      enable = true;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
-            targets = {
-                grub.enable = false;
-            };
+      targets = {
+        grub.enable = false;
+      };
 
-            cursor = {
-                package = pkgs.bibata-cursors;
-                name = "Bibata-Modern-Ice";
-                size = 24;
-            };
+      cursor = {
+				package = pkgs.rose-pine-cursor;
+				name = "BreezeX-RosePine-Linux";
+        size = 24;
+      };
 
-            fonts = {
-                monospace = {
-                    package = pkgs.nerd-fonts.jetbrains-mono;
-                    name = "JetBrainsMono Nerd Font Mono";
-                };
-
-                sansSerif = {
-                    package = pkgs.dejavu_fonts;
-                    name = "DejaVu Sans";
-                };
-
-                serif = {
-                    package = pkgs.dejavu_fonts;
-                    name = "DejaVu Serif";
-                };
-
-                sizes = {
-                    applications = 12;
-                    terminal = 15;
-                    desktop = 10;
-                    popups = 10;
-                };
-            };
-
-            opacity = {
-                applications = 1.0;
-                terminal = 1.0;
-                desktop = 1.0;
-                popups = 1.0;
-            };
-
-            polarity = "dark";
-
+      fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrainsMono Nerd Font Mono";
         };
+
+        sansSerif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Sans";
+        };
+
+        serif = {
+          package = pkgs.dejavu_fonts;
+          name = "DejaVu Serif";
+        };
+
+        sizes = {
+          applications = 12;
+          terminal = 15;
+          desktop = 10;
+          popups = 10;
+        };
+      };
+
+      opacity = {
+        applications = 1.0;
+        terminal = 1.0;
+        desktop = 1.0;
+        popups = 1.0;
+      };
+
+      polarity = "dark";
     };
-
+  };
 }
-
