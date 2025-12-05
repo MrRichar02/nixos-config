bb3eb4252fd279f932c239cf7b8203df00724c0c
MrRichar02 <richarmamadi03@gmail.com>
2025-12-04 20:53:13 -0500

    commit

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stylix.url = "github:danth/stylix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    stylix.url = "github:danth/stylix/release-25.11";
   };
 
   outputs = { nixpkgs, ... } @ inputs:
