b884e6c5939e7f13f50fe50b27a1e2bc3d8c5a7f
MrRichar02 <richarmamadi03@gmail.com>
2026-02-03 18:31:21 -0500

    commit

--- hosts/thinkpad-x1/hardware-configuration.nix
+++ hosts/thinkpad-x1/hardware-configuration.nix
@@ -14,12 +14,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1b2e6e18-8da6-42bb-9e74-ee60067100cb";
-      fsType = "ext4";
+    { device = "/dev/disk/by-uuid/33a2503a-634b-4744-9fa9-446ba6b5ee69";
+      fsType = "btrfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D97D-FE9C";
+    { device = "/dev/disk/by-uuid/353E-0E23";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
@@ -30,14 +30,6 @@
       size = 8 * 1024;
     }
   ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
